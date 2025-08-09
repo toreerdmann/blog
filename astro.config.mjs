185d1829b6cdb933c305f67f254505e808b4840e
Tore Erdmann <tore.erdmann@gmail.com>
2025-08-09 16:05:41 +0200

    fix base and rename blog -> posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,5 @@ const isProd = process.env.NODE_ENV === "production"
 // https://astro.build/config
 export default defineConfig({
     site: isProd ? "https://toreerdmann.github.io" : 'https://example.com',
-    base: "/blog",
 	integrations: [mdx(), sitemap()],
 });
--- src/content.config.ts
+++ src/content.config.ts
@@ -2,8 +2,8 @@ import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+	// Load Markdown and MDX files in the `src/content/posts/` directory.
+	loader: glob({ base: './src/content/posts', pattern: '**/*.{md,mdx}' }),
 	// Type-check frontmatter using a schema
 	schema: ({ image }) =>
 		z.object({
