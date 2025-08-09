31aeb7d4fd6bd4dec8b41aaf4bee702fc313f19d
Tore Erdmann <tore.erdmann@gmail.com>
2025-08-09 15:57:25 +0200

    set base

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,6 @@ const isProd = process.env.NODE_ENV === "production"
 // https://astro.build/config
 export default defineConfig({
     site: isProd ? "https://toreerdmann.github.io" : 'https://example.com',
-    base: "blog",
+    base: "/blog",
 	integrations: [mdx(), sitemap()],
 });
