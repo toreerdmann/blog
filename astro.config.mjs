94078faccca2ff5964c6068aedaceb2904b3d4c6
Tore Erdmann <tore.erdmann@gmail.com>
2025-08-09 15:42:13 +0200

    fix typo

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,6 @@ const isProd = process.env.NODE_ENV === "production"
 // https://astro.build/config
 export default defineConfig({
     site: isProd ? "https://toreerdmann.github.io" : 'https://example.com',
+    base: isProd ? "blog" : undefined,
 	integrations: [mdx(), sitemap()],
 });
